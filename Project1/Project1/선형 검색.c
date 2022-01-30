f82d1cf64af2dd6d42a2ccacc6091c2ec44e212a
YOUNGDUCKPC\98tom <tommykimyd98@gmail.com>
2022-01-31 03:34:40 +0900

    선형 검색, 그래픽 추가

--- Project1/Project1/선형 검색.c
+++ Project1/Project1/선형 검색.c
@@ -1,7 +1,37 @@
 #include<stdio.h>
 
+void show_search(int a[], int n, int i_a) {
+	if (i_a == 0) {
+		printf("%3c|", ' ');
+		for (int i = 0; i < n; i++) {
+			printf("%3d", a[i]);
+		}
+		printf("\n");
+		for (int i = 0; i < 3*(n+1); i++) {
+			printf("%c", '-');	
+		}
+		printf("\n");
+	}
+	printf("%3c", ' ');
+	for (int i = 0; i < n; i++) {
+		if (a[i] == a[i_a]) {
+			printf("%3c", '*');
+		}
+		else {
+			printf("   ");
+		}
+	}
+
+	printf("\n%3d|", i_a);
+	for (int i = 0; i < n; i++) {
+		printf("%3d", i);
+	}
+	printf("\n");
+}
+
 int linear_search(int a[], int n) {
 	for (int i = 0; i < n; i++) {
+		show_search(a, n, i);
 		if (a[i] == a[n]) {
 			return i;
 			break;
@@ -18,6 +48,6 @@ int main() {
 		printf("fail");
 	}
 	else {
-		printf("a[%d]", i);
+		printf("\na[%d]", i);
 	}
 }

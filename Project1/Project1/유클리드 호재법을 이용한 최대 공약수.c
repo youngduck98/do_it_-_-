e7913e2b916e265fd94bc6f07e53a12a2102be81
YOUNGDUCKPC\98tom <tommykimyd98@gmail.com>
2022-02-03 13:09:53 +0900

    유클리드 호재법을 이용한 최대공약수 함수와 배열 내의 최대공약수를 구하는 함수 추가

--- Project1/Project1/유클리드 호재법을 이용한 최대 공약수.c
+++ Project1/Project1/유클리드 호재법을 이용한 최대 공약수.c
@@ -9,6 +9,21 @@ int uclid_gcf(int x, int y) {
 	return uclid_gcf(y, x % y);
 }
 
+int gcd_ary(int a[], int n) {
+	if (n == 1) {
+		return *a;
+	}
+	else if (n == 2) {
+		return uclid_gcf(*(a + 1), *(a));
+	}
+	else {
+		*(a + 1) = uclid_gcf(*(a + 1), *(a));
+		return gcd_ary(a + 1, --n);
+	}
+}
+
 int main() {
 	printf("%d", uclid_gcf(25, 15));
+	int a[3] = { 9,18,30 };
+	printf("\n%d", gcd_ary(a, 3));
 }
